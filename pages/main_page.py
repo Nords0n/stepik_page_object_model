a39ed20b67467fd2128e8c0f32b87e200806684c
Slava <slavashiman@09gmail.com>
2024-07-09 04:33:27 +0300

    Added pass to class

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,9 +5,11 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser3.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+    # def go_to_login_page(self):
+    #     login_link = self.browser3.find_element(*MainPageLocators.LOGIN_LINK)
+    #     login_link.click()
+    #
+    # def should_be_login_link(self):
+    #     assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    pass
