44783f7ca0b11bd301e0851f8b282cfd49ba45a0
Slava <slavashiman@09gmail.com>
2024-07-10 22:13:26 +0300

    Added new method - user registration

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,10 @@
+from faker import Faker
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
+fake = Faker()
 
 class LoginPage(BasePage):
     def should_check_login_page(self):
@@ -19,3 +23,23 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self):
+        f_email = fake.ascii_free_email()
+        f_password = fake.password()
+
+        WebDriverWait(self.browser3, 10).until(
+            EC.presence_of_element_located(LoginPageLocators.EMAIL_ADDRESS_REGISTER)
+        ).send_keys(f_email)
+
+        WebDriverWait(self.browser3, 10).until(
+            EC.presence_of_element_located(LoginPageLocators.PASSWORD_REGITSTER)
+        ).send_keys(f_password)
+
+        WebDriverWait(self.browser3, 10).until(
+            EC.presence_of_element_located(LoginPageLocators.PASSWORD_CONF_REGITSTER)
+        ).send_keys(f_password)
+
+        WebDriverWait(self.browser3, 10).until(
+            EC.presence_of_element_located(LoginPageLocators.REGISTER_BUTTON)
+        ).click()
